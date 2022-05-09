d28bc9383c2bffab87100f8ed44b35e10d005355
Cinmin Chen <cinminchen212@gmail.com>
2022-05-09 21:41:58 +0800

    Main Page
    Add Scroll down btn

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,7 @@
-// JavaScript Document
-$(document).ready(function () {
-
-    $(".topNav_mobile").hide();
-    $(".menu").click(function () {
-        $(".topNav_mobile").slideToggle();
+$(function () {
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
     });
-
 });
-
-
 
