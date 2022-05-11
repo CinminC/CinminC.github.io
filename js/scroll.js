954f82cec93eaf24e6af6bb198613757c7e769dc
Cinmin Chen <cinminchen212@gmail.com>
2022-05-11 21:35:48 +0800

    Adjust Scroll
    call to scroll on main page only show screen size >768

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 $(document).ready(function () {
 
-    $(".topNav_mobile").hide();
-    $(".menu").click(function () {
-        $(".topNav_mobile").slideToggle();
+    $('a[href*=#]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
     });
 
 });
